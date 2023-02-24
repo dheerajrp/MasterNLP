f97acb572a4d7eb6e1dacd00da620e74f3a3ce57
Dheeraj RP <dheerajrp66@gmail.com>
2023-02-24 12:51:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//const homeBtn = document.getElementById("home");
+//
+//homeBtn.addEventListener("click", function(event) {
+//  event.preventDefault(); // Prevent default behavior of the link
+//  window.location.href = "concepts.html"; // Redirect to the home page (change "index.html" to your home page filename)
+//});
